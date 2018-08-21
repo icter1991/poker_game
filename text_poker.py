57a9b539c953796066312a529a0d9b1c5b3e2b65
Sebastian Krzeminski <icter1991@gmail.com>
2018-08-21 22:20:33 +0100

    0.0.1.3

---
+++ text_poker.py
@@ -0,0 +1,8 @@
+# text_poker.py -- video dice poker using a text-based interface.
+
+from pokerdie import PokerApp
+from textpoker import TextInterface
+
+inter = TextInterface()
+app = PokerApp(inter)
+app.run()
